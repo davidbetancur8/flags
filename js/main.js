0dca740b7a11ea310e56c8f963e0f8ef1794853a
David Betancur <david.bet.san@gmail.com>
2019-01-22 22:53:51 +0100

    flag on hoover

--- js/main.js
+++ js/main.js
@@ -9,6 +9,15 @@ names_a2["SS"] = "South Sudan"
 var basic_choropleth = new Datamap({
   element: document.getElementById("container"),
   projection: 'mercator',
+  geographyConfig: {
+    highlightBorderColor: '#bada55',
+    popupTemplate: function hover_flag(geography) {
+      const code = Object.keys(names_a2).find(key => names_a2[key] === geography.properties.name);
+      image_template = "<img src=" + "https://www.countryflags.io/" + code + "/shiny/64.png" + ">"
+      text_template = "<p>" + geography.properties.name + "</p>"
+      return image_template + geography.properties.name
+    }
+  },
   done: function(datamap) {
     datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
       show_flag(geography.properties.name);
@@ -27,4 +36,6 @@ function show_flag(country_name) {
   }
   console.log(code)
 }
+
+
 
