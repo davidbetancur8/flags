63fdc9ff07873016ef295a478b90303514d4321e
David Betancur <david.bet.san@gmail.com>
2019-02-02 11:53:41 +0100

    arreglos finales

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+// names_a2 y data son diccionarios que estan en el directorio src. Estos estan en archivos js
+// y se cargan en el html junto con este archivo. Estos diccionarios fueron creados usando el
+// archivo generate_data.ipynb
 
+// Nombres que hay que cambiar en el diccionario
+// =================================================
 names_a2["US"] = "United States of America"
 names_a2["TZ"] = "United Republic of Tanzania"
 names_a2["CG"] = "Republic of the Congo"
@@ -9,38 +14,16 @@ names_a2["IR"] = "Iran"
 names_a2["SY"] = "Syria"
 names_a2["MM"] = "Myanmar"
 names_a2["LA"] = "Laos"
+// ===================================================
 
 
-Chart.defaults.global.defaultFontSize = 15;
-Chart.defaults.scale.gridLines.display = false;
-
+// funciones del mapa de banderas
+// ===================================================
 function unique(a) {
   return a.sort().filter(function(value, index, array) {
       return (index === 0) || (value !== array[index-1]);
   });
 }
-
-
-var basic_choropleth = new Datamap({
-  element: document.getElementById("container"),
-  projection: 'mercator',
-  geographyConfig: {
-    highlightBorderColor: '#bada55',
-    popupTemplate: function hover_flag(geography) {
-      const code = Object.keys(names_a2).find(key => names_a2[key] === geography.properties.name);
-      image_template = "<img src=" + "https://www.countryflags.io/" + code + "/shiny/64.png" + ">"
-      text_template = '<p style="background-color: #d0d4db; display: inline;">' + geography.properties.name + "</p>"
-      return image_template + text_template
-    }
-  },
-  done: function(datamap) {
-    datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-      show_flag(geography.properties.name);
-    });
-}
-});
-
-basic_choropleth.updateChoropleth(data);
 
 function show_flag(country_name) {
 
@@ -102,28 +85,6 @@ function update_images_borders(a2) {
   }
   
 }
-var slider_red = document.getElementById("slider_red");
-var output_red = document.getElementById("p_red");
-output_red.innerHTML = slider_red.value;
-
-var slider_green = document.getElementById("slider_green");
-var output_green = document.getElementById("p_green");
-output_red.innerHTML = slider_green.value;
-
-var slider_blue = document.getElementById("slider_blue");
-var output_blue = document.getElementById("p_blue");
-output_red.innerHTML = slider_blue.value;
-
-
-slider_red.oninput = function() {
-  output_red.innerHTML = this.value;
-}
-slider_green.oninput = function() {
-  output_green.innerHTML = this.value;
-}
-slider_blue.oninput = function() {
-  output_blue.innerHTML = this.value;
-}
 
 function update_map() {
   r = slider_red.value
@@ -147,7 +108,65 @@ function update_map() {
   }
   console.log(data)
 }
+// ===================================================
 
+
+// Creacion del mapa
+// ===================================================
+var basic_choropleth = new Datamap({
+  element: document.getElementById("container"),
+  projection: 'mercator',
+  geographyConfig: {
+    highlightBorderColor: '#bada55',
+    popupTemplate: function hover_flag(geography) {
+      const code = Object.keys(names_a2).find(key => names_a2[key] === geography.properties.name);
+      image_template = "<img src=" + "https://www.countryflags.io/" + code + "/shiny/64.png" + ">"
+      text_template = '<p style="background-color: #d0d4db; display: inline;">' + geography.properties.name + "</p>"
+      return image_template + text_template
+    }
+  },
+  done: function(datamap) {
+    datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+      show_flag(geography.properties.name);
+    });
+}
+});
+
+basic_choropleth.updateChoropleth(data);
+// ===================================================
+
+// Sliders
+// ===================================================
+var slider_red = document.getElementById("slider_red");
+var output_red = document.getElementById("p_red");
+output_red.innerHTML = slider_red.value;
+
+var slider_green = document.getElementById("slider_green");
+var output_green = document.getElementById("p_green");
+output_red.innerHTML = slider_green.value;
+
+var slider_blue = document.getElementById("slider_blue");
+var output_blue = document.getElementById("p_blue");
+output_red.innerHTML = slider_blue.value;
+// ===================================================
+
+// Slider functions
+// ===================================================
+slider_red.oninput = function() {
+  output_red.innerHTML = this.value;
+}
+slider_green.oninput = function() {
+  output_green.innerHTML = this.value;
+}
+slider_blue.oninput = function() {
+  output_blue.innerHTML = this.value;
+}
+// ===================================================
+
+// Radar Chart
+// ===================================================
+Chart.defaults.global.defaultFontSize = 15;
+Chart.defaults.scale.gridLines.display = false;
 
 var ctx = document.getElementById("canvas1").getContext('2d');
 
@@ -232,3 +251,4 @@ var myChart = new Chart(ctx, {
   },
 
 });
+// ===================================================
